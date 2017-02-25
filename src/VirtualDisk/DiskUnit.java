3fbad7b039f5874713dd543d93b010870ad16262
rexkater <rexkater@Rex-HP>
2017-02-25 00:20:14 -0400

    First Phase
    Everything working right up until now.
    Added respective comments to all importan methods.

--- src/VirtualDisk/DiskUnit.java
+++ src/VirtualDisk/DiskUnit.java
@@ -10,16 +10,17 @@ import Exceptions.NonExistingDiskException;
 
 public class DiskUnit {
 	
-	private static final int DEFAULT_CAPACITY = 1024;  // default number of blocks     
-	private static final int DEFAULT_BLOCK_SIZE = 256; // default number of bytes per block
+	private static final int DEFAULT_CAPACITY = 1024;  // Default number of blocks.    
+	private static final int DEFAULT_BLOCK_SIZE = 256; // Default number of bytes per block.
 	// private static final int OFFSET = 8; // 8 bits offsets for size and capacity
-	private int capacity; // number of blocks of current disk instance
-	private int blockSize; // size of each block of current disk instance
-	private RandomAccessFile disk; // the file representing the simulated  disk, where all the disk blocks are stored
+	private int capacity; // Number of blocks of current disk instance.
+	private int blockSize; // Size of each block of current disk instance.
+	private RandomAccessFile disk; // File representing the simulated  disk, where all the disk blocks are stored.
 
-	/** private CONSTRUCTOR
-    @param name is the name of the disk
-	  **/
+	/** Private constructor to initiate the 
+	 * RAF representing the virtual disk.
+     * @param name is the name of the disk.
+	 */
 	
 	private DiskUnit(String name) {
 	     try {
@@ -33,11 +34,11 @@ public class DiskUnit {
 	/**
 	 * Turns on an existing disk unit whose name is given. If successful, it makes
 	 * the particular disk unit available for operations suitable for a disk unit.
-	 * @param name the name of the disk unit to activate
-	 * @return the corresponding DiskUnit object
+	 * @param name is the name of the disk unit to activate.
+	 * @return the corresponding DiskUnit object.
 	 * @throws NonExistingDiskException whenever no
 	 *    ¨disk¨ with the specified name is found.
-	*/
+	 */
 	
 	public static DiskUnit mount(String name) throws NonExistingDiskException {
 		File file = new File(name);
@@ -66,11 +67,11 @@ public class DiskUnit {
 	 * as having default capacity (number of blocks), each of default
 	 * size (number of bytes). Those values are: DEFAULT_CAPACITY and
 	 * DEFAULT_BLOCK_SIZE. The created disk is left as in off mode.
-	 * @param name the name of the file that is to represent the disk.
+	 * @param name is the name of the file that is to represent the disk.
 	 * @throws ExistingDiskException whenever the name attempted is
 	 * already in use.
-	 * @throws InvalidParameterException 
-	*/
+	 * @throws InvalidParameterException for parameters of any type but string.
+	 */
 	public static void createDiskUnit(String name) throws ExistingDiskException, InvalidParameterException {
 		createDiskUnit(name, DEFAULT_CAPACITY, DEFAULT_BLOCK_SIZE);
 	}
@@ -80,13 +81,13 @@ public class DiskUnit {
 	 * as with the specified capacity (number of blocks), each of specified
 	 * size (number of bytes).  The created disk is left as in off mode.
 	 * @param name the name of the file that is to represent the disk.
-	 * @param capacity number of blocks in the new disk
-	 * @param blockSize size per block in the new disk
+	 * @param capacity is the number of blocks in the new disk.
+	 * @param blockSize represents the size per block in the new disk.
 	 * @throws ExistingDiskException whenever the name attempted is
 	 * already in use.
 	 * @throws InvalidParameterException whenever the values for capacity
-	 *  or blockSize are not valid according to the specifications
-	*/
+	 *  or blockSize are not valid according to the specifications.
+	 */
 	public static void createDiskUnit(String name, int capacity, int blockSize) throws ExistingDiskException, InvalidParameterException{
 	    File file = new File(name);
 	    
@@ -118,10 +119,13 @@ public class DiskUnit {
 	}
 	
 	/**
-	 * 
-	 * @param disk
-	 * @param capacity
-	 * @param blockSize
+	 * Reserves a certain amount of space in the disk for the capacity and blockSize.
+	 * This space CAN'T be worked with or used while writing or reading on a certain disk.
+	 * It reserves the whole first block, even though only the first 8 bits are needed.
+	 * @param disk represents the specific disk in which the space will be reserved.
+	 * @param capacity first value to be stored on the reserved block of the disk.
+	 * @param blockSize second value to be stored on the reserved block of the disk,
+	 * right after the capacity value.
 	 */
 	
 	private static void reserveDiskSpace(RandomAccessFile disk, int capacity, int blockSize) {
@@ -145,18 +149,23 @@ public class DiskUnit {
 	
 	
 	/**
-	 * 
-	 * @param blockNum
-	 * @param b
-	 * @throws InvalidBlockNumberException
-	 * @throws InvalidBlockException
+	 * Writes the content of block b into the disk block corresponding to blockNum; 
+	 * that is, whatever is the actual content of the disk block corresponding to the 
+	 * specified block number (blockNum) is changed to (or overwritten by) that of b 
+	 * in the current disk instance.
+	 * @param blockNum is the number of a specific block of the disk. 
+	 * @param b represents the data that will be written on the disk.
+	 * @throws InvalidBlockNumberException for a blockNum of zero (block 0 can't be written)
+	 * or lower, or a blockNum higher than the capacity of the disk.
+	 * @throws InvalidBlockException when the given VirtualDiskBlock parameter is null or if 
+	 * block instance does not match the block size of the current disk instance.
 	 */
 	
 	public void write(int blockNum, VirtualDiskBlock b) throws InvalidBlockNumberException, InvalidBlockException {		
 		if(blockNum <= 0 || blockNum > capacity)
 			throw new InvalidBlockNumberException();
 		
-		if(b==null)
+		if(b==null || b.getCapacity() != blockSize)
 			throw new InvalidBlockException();
 		
 		try { 
@@ -170,18 +179,22 @@ public class DiskUnit {
 	}
 	
 	/**
-	 * 
-	 * @param blockNum
-	 * @param b
-	 * @throws InvalidBlockNumberException
-	 * @throws InvalidBlockException
+	 * Reads a given block from the disk. The content of the specified disk 
+	 * block (identified by its number – blockNum) is copied as the new content 
+	 * of the current instance of block being referenced by parameter b.
+	 * @param blockNum is the number of a specific block of the disk. 
+	 * @param b represents the data that will be read from the disk.
+	 * @throws InvalidBlockNumberException for a blockNum lower than zero, 
+	 * or equal/higher than the capacity of the disk.
+	 * @throws InvalidBlockException when the given VirtualDiskBlock parameter is null or if 
+	 * block instance does not match the block size of the current disk instance.
 	 */
 	
 	public void read(int blockNum, VirtualDiskBlock b) throws InvalidBlockNumberException, InvalidBlockException{
 		if(blockNum < 0 || blockNum >= capacity)
 			throw new InvalidBlockNumberException();
 		
-		if(b==null)
+		if(b==null || b.getCapacity() != blockSize)
 			throw new InvalidBlockException();
 	
 		try { 
@@ -199,8 +212,8 @@ public class DiskUnit {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Method to get the blockSize value.
+	 * @return blockSize instance variable.
 	 */
 
 	public int getBlockSize() {
@@ -208,8 +221,8 @@ public class DiskUnit {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Method to get the disk's capacity value.
+	 * @return capacity instance variable.
 	 */
 
 	public int getCapacity() {
@@ -217,9 +230,8 @@ public class DiskUnit {
 	}
 	
 	/**
-	 * Formats the disk.
-	 * This operation visits every physical block in the disk and fills 
-	 * with zeroes all those that are valid. 
+	 * Formats the disk. This operation visits every “physical block”, 
+	 * except block 0, in the disk and fills with zeroes all those blocks.
 	 * @throws IOException 
 	 */
 	
@@ -239,7 +251,8 @@ public class DiskUnit {
 	
 	  /** 
 	   * Simulates shutting-off the disk. Just closes the corresponding RAF. 
-	   **/
+	   */
+	
 	  public void shutdown() {
 	    try {
 	       disk.close();
--- src/VirtualDisk/VirtualDiskBlock.java
+++ src/VirtualDisk/VirtualDiskBlock.java
@@ -1,45 +1,45 @@
 package VirtualDisk;
 
 public class VirtualDiskBlock {
-	private static final int DEFCAP = 256; // default number of bytes per block
-	private byte[] blockUnit; // store the bytes array virtually for read and write operations
+	private static final int DEFCAP = 256; // Default number of bytes per block.
+	private byte[] blockUnit; // Bytes array for the virtual read and write operations.
 	
 	/**
-	 * 
+	 * Constructor that uses the DEFCAP on the next constructor, which has a capacity parameter.
 	 */
 	public VirtualDiskBlock(){ 
 		this(DEFCAP);
 	}
 	
 	/**
-	 * 
-	 * @param blockCapacity
+	 * Constructor to initiate the blockUnit array.
+	 * @param blockCapacity is the number used for the initiation.
 	 */
 	public VirtualDiskBlock(int blockCapacity) {
 		blockUnit = new byte[blockCapacity];
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Method to get the the blockUnit's capacity value.
+	 * @return blockUnit.length which represents the array's capacity.
 	 */
 	public int getCapacity() {
 		return blockUnit.length;
 	}
 
 	/**
-	 * 
-	 * @param i
-	 * @return
+	 * Gets an array's value at an specific slot.
+	 * @param i represents the index from which the element will be picked.
+	 * @return blockUnit's element at index i.
 	 */
 	public byte getElement(int i) {
 		return blockUnit[i];
 	}
 
 	/**
-	 * 
-	 * @param i
-	 * @param b
+	 * Sets an indicated element at the specified slot, replacing the old element.
+	 * @param i represents the index in which the parameter b will be replacing the old element.
+	 * @param b is the element that will replace the old one at the slot/index specified by i.
 	 */
 	public void setElement(int i, byte b) {
 		blockUnit[i] = b;
